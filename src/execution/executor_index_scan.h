82891486d368fd7ddaaa7ce92047e0d8b3998351
FuyukiVila <1642421711@qq.com>
2025-05-27 00:33:40 +0800

    feat(index): 添加索引名称获取和文件打开逻辑

--- src/execution/executor_index_scan.h
+++ src/execution/executor_index_scan.h
@@ -48,6 +48,12 @@ class IndexScanExecutor : public AbstractExecutor {
         fh_ = sm_manager_->fhs_.at(tab_name_).get();
         cols_ = tab_.cols;
         len_ = cols_.back().offset + cols_.back().len;
+        index_name_ = sm_manager_->get_ix_manager()->get_index_name(tab_name_, index_col_names_);
+        if (!sm_manager->ihs_.count(index_name_)) {
+            // 如果没有打开则打开文件
+            sm_manager->ihs_.emplace(index_name_,
+                                     sm_manager_->get_ix_manager()->open_index(tab_name_, index_col_names));
+        }
 
         std::function<CompOp(CompOp)> swap_op = [](CompOp op) {
             switch (op) {
@@ -82,8 +88,7 @@ class IndexScanExecutor : public AbstractExecutor {
 
     void beginTuple() override {
         // 构建索引查询范围
-        auto ih =
-            sm_manager_->ihs_.at(index_name_).get();
+        auto ih = sm_manager_->ihs_.at(index_name_).get();
         // 从条件中提取索引键的范围
         char *lower_key = new char[index_meta_.col_tot_len];
         char *upper_key = new char[index_meta_.col_tot_len];
