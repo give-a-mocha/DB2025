a09e4e405452070b1470a0ff6a2059aaa5ff0f6f
lin_jy <2221650642@qq.com>
2025-06-20 19:07:33 +0800

    feat(common): 添加线程池功能
    - 新增 ThreadPool 类，实现了线程池的基本功能
    - 线程池可以创建指定数量的线程，并管理它们的生命周期
    - 提供了任务提交接口，支持异步执行任务并获取结果
    - 线程池在销毁时会等待所有线程完成当前任务

---
+++ src/common/ThreadPool.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <stdexcept>
+#include <thread>
+#include <vector>
+
+class ThreadPool {
+   public:
+    ThreadPool(size_t);
+    ~ThreadPool();
+
+    template <class F, class... Args>
+    auto submit(F&& f, Args&&... args) -> std::future<decltype(f(args...))>;
+    
+   private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()> > tasks;
+    std::mutex queue_mutex;
+    std::condition_variable condition;
+    bool stop;
+};
+
+inline ThreadPool::ThreadPool(size_t threads) : stop(false) {
+    for (size_t i = 0; i < threads; ++i)
+        workers.emplace_back([this] {
+            for (;;) {
+                std::function<void()> task;
+
+                {
+                    std::unique_lock<std::mutex> lock(this->queue_mutex);
+                    this->condition.wait(lock, [this] { return this->stop || !this->tasks.empty(); });
+                    if (this->stop && this->tasks.empty()) return;
+                    task = std::move(this->tasks.front());
+                    this->tasks.pop();
+                }
+
+                task();
+            }
+        });
+}
+
+template <class F, class... Args>
+auto ThreadPool::submit(F&& f, Args&&... args) -> std::future<decltype(f(args...))> {
+    auto task = std::make_shared<std::packaged_task<decltype(f(args...))()> >(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+    std::future<decltype(f(args...))> res = task->get_future();
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+        if (stop) throw std::runtime_error("submit on stopped ThreadPool");
+        tasks.emplace([task]() { (*task)(); });
+    }
+    condition.notify_one();
+    return res;
+}
+
+inline ThreadPool::~ThreadPool() {
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+        stop = true;
+    }
+    condition.notify_all();
+    for (std::thread& worker : workers) worker.join();
+}
+
+#endif
