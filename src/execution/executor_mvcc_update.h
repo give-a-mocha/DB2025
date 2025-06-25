9a41e168fe1811b5dffbe9d772b2f5cac17bdb2d
Masttf <1124677368@qq.com>
2025-06-25 22:23:54 +0800

    fix:insert/update错误

--- src/execution/executor_mvcc_update.h
+++ src/execution/executor_mvcc_update.h
@@ -156,14 +156,16 @@ class MvccUpdateExecutor : public AbstractExecutor {
             }
             auto rid_ = fh_->insert_record(new_rec->data, context_);
             txn_mgr_->get_lock_manager()->lock_exclusive_on_record(context_->txn_, rid_, fh_->GetFd());
+            // 添加日志要在插入索引之后，因为abort会回滚索引
+            if (!sm_manager_->insert_index(tab_name_, *new_rec, rid_, context_)) {
+                fh_->delete_record(rid_, context_);
+                txn_mgr_->abort(context_, context_->log_mgr_);
+                throw RMDBError("Failed to insert into index, rolled back record insertion at " + getType());
+            }
             std::vector<Value> values_ = convert_record_to_values(new_rec, tab_.cols);
             txn_mgr_->add_insert_undo_log(context_->txn_, rid_, std::move(values_));
             context_->txn_->append_write_record(std::make_unique<WriteRecord>(WType::INSERT_TUPLE, tab_.name, rid_, *new_rec));
             context_->log_mgr_->add_insert_log(context_->txn_->get_transaction_id(), *new_rec, rid_, tab_.name);
-            if (!sm_manager_->insert_index(tab_name_, *new_rec, rid_, context_)) {
-                txn_mgr_->abort(context_, context_->log_mgr_);
-                throw RMDBError("Failed to insert into index, rolled back record insertion at " + getType());
-            }
         }
         return nullptr;
     }
