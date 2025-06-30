213105ea1e32941bf816752415b86e6c416585d8
lin_jy <2221650642@qq.com>
2025-06-30 21:25:23 +0800

    perf(执行管理器): 优化查询性能并暂时禁用索引操作
    - 修改 CMakeLists.txt，启用 O3 优化标志
    - 在 print.hpp 中添加条件编译指令，可选择禁用打印功能
    - 在 execution_manager.cpp 中注释掉创建和删除索引的操作

--- src/common/print.hpp
+++ src/common/print.hpp
@@ -7,6 +7,10 @@
 
 #include "common/Format.h"
 
+// #define PrintEnable
+
+#ifdef PrintEnable
+
 template <typename... Args>
 void print(std::string_view fmt_str, Args &&...args) {
     std::cout << util::format(std::string(fmt_str), std::forward<Args>(args)...);
@@ -34,5 +38,24 @@ void ERROR(std::string_view fmt_str, Args &&...args) {
     std::clog << "\033[0m\033[1;31m" << util::format(std::string(fmt_str), std::forward<Args>(args)...) << "\033[0m"
               << std::endl;
 }
+
+#else
+
+template <typename... Args>
+void print(std::string_view fmt_str, Args &&...args) {}
+
+template <typename... Args>
+void println(std::string_view fmt_str, Args &&...args) {}
+
+template <typename... Args>
+void INFO(std::string_view fmt_str, Args &&...args) {}
+
+template <typename... Args>
+void WARN(std::string_view fmt_str, Args &&...args) {}
+
+template <typename... Args>
+void ERROR(std::string_view fmt_str, Args &&...args) {}
+
+#endif
 
 #endif
