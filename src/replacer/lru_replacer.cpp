fde68ac11cdabfb4f794ededfdebcda14cb2c2c5
FuyukiVila <1642421711@qq.com>
2025-07-04 15:57:28 +0800

    fix: 注释掉unpin函数中的满了检查以简化逻辑

--- src/replacer/lru_replacer.cpp
+++ src/replacer/lru_replacer.cpp
@@ -72,9 +72,9 @@ void LRUReplacer::unpin(frame_id_t frame_id) {
     // std::scoped_lock lock{latch_};
 
     // 满了
-    if (LRUlist_.size() >= max_size_) {
-        return;
-    }
+    // if (LRUlist_.size() >= max_size_) {
+    //     return;
+    // }
 
     // 选择一个frame取消固定
     if (LRUhash_.find(frame_id) != LRUhash_.end()) return;
