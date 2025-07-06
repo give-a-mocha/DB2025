38fe84fb7956944d2c35435695be24c2e46cba6d
Masttf <1124677368@qq.com>
2025-07-06 21:19:54 +0800

    Update:采用wait-die的锁策略

--- src/transaction/concurrency/lock_manager.cpp
+++ src/transaction/concurrency/lock_manager.cpp
@@ -143,7 +143,7 @@ bool LockManager::lock_shared_on_record(Transaction* txn, const Rid& rid, int ta
  * @throws TransactionAbortException 如果事务状态不允许加锁
  */
 bool LockManager::lock_exclusive_on_record(Transaction* txn, const Rid& rid, int tab_fd) {
-    std::scoped_lock<std::mutex> lock(latch_);  // 1. Acquire global latch
+    std::unique_lock<std::mutex> lock(latch_);  // 1. Acquire global latch
 
     // 创建锁数据标识符( 行级锁
     LockDataId lock_data_id(tab_fd, rid, LockDataType::RECORD);
@@ -193,78 +193,78 @@ bool LockManager::lock_exclusive_on_record(Transaction* txn, const Rid& rid, int
         return true;
     } else {
         // no-wait策略
-        return false;
+        // return false;
 
         // wait-die策略
-        // request_queue.request_queue_.push_back(current_request);
-        // auto current_request_it = std::prev(request_queue.request_queue_.end());
+        request_queue.request_queue_.push_back(current_request);
+        auto current_request_it = std::prev(request_queue.request_queue_.end());
 
-        // while (true) {
+        while (true) {
 
-        //     request_queue.cv_.wait(lock, [&] {
-        //         if (txn->get_state() == TransactionState::ABORTED) {
-        //             return true;
-        //         }
-        //         bool can_grant = true;
-        //         for (const auto& req : request_queue.request_queue_) {
-        //             if (req.granted_) {
-        //                 if(req.txn_id_ < txn->get_transaction_id()) {
-        //                     can_grant = true;
-        //                     break;
-        //                 }
-        //                 if (req.txn_id_ != txn->get_transaction_id()) {
-        //                     can_grant = false;
-        //                     break;
-        //                 }
-        //                 if (req.txn_id_ == txn->get_transaction_id() && req.lock_mode_ == LockMode::SHARED) {
-        //                     can_grant = false;
-        //                     break;
-        //                 }
-        //             }
-        //         }
-        //          return can_grant;
-        //     });
+            request_queue.cv_.wait(lock, [&] {
+                if (txn->get_state() == TransactionState::ABORTED) {
+                    return true;
+                }
+                bool can_grant = true;
+                for (const auto& req : request_queue.request_queue_) {
+                    if (req.granted_) {
+                        if(req.txn_id_ < txn->get_transaction_id()) {
+                            can_grant = true;
+                            break;
+                        }
+                        if (req.txn_id_ != txn->get_transaction_id()) {
+                            can_grant = false;
+                            break;
+                        }
+                        if (req.txn_id_ == txn->get_transaction_id() && req.lock_mode_ == LockMode::SHARED) {
+                            can_grant = false;
+                            break;
+                        }
+                    }
+                }
+                return can_grant;
+            });
 
-        //     bool is_aborted = false;
-        //     if(txn->get_state() == TransactionState::ABORTED) {
-        //         is_aborted = true;
-        //     }
-        //     for (const auto& req : request_queue.request_queue_) {
-        //          if (req.granted_ && req.lock_mode_ == LockManager::LockMode::EXCLUSIVE) {
-        //             if(req.txn_id_ < txn->get_transaction_id()) {
-        //                 is_aborted = true;
-        //                 break;
-        //             }
-        //          }
-        //     }
+            bool is_aborted = false;
+            if(txn->get_state() == TransactionState::ABORTED) {
+                is_aborted = true;
+            }
+            for (const auto& req : request_queue.request_queue_) {
+                 if (req.granted_ && req.lock_mode_ == LockManager::LockMode::EXCLUSIVE) {
+                    if(req.txn_id_ < txn->get_transaction_id()) {
+                        is_aborted = true;
+                        break;
+                    }
+                 }
+            }
 
-        //     if (is_aborted) {
-        //          if (current_request_it != request_queue.request_queue_.end() && !current_request_it->granted_) {
-        //              request_queue.request_queue_.erase(current_request_it);
-        //          }
-        //         return false;
-        //     }
+            if (is_aborted) {
+                 if (current_request_it != request_queue.request_queue_.end() && !current_request_it->granted_) {
+                     request_queue.request_queue_.erase(current_request_it);
+                 }
+                return false;
+            }
 
-        //     bool still_conflict = false;
-        //     for (const auto& req : request_queue.request_queue_) {
-        //          if (req.granted_) {
-        //              if (req.txn_id_ != txn->get_transaction_id()) {
-        //                  still_conflict = true;
-        //                  break;
-        //              }
-        //              if (req.txn_id_ == txn->get_transaction_id() && req.lock_mode_ == LockMode::SHARED) {
-        //                  still_conflict = true;
-        //                  break;
-        //              }
-        //          }
-        //     }
+            bool still_conflict = false;
+            for (const auto& req : request_queue.request_queue_) {
+                 if (req.granted_) {
+                     if (req.txn_id_ != txn->get_transaction_id()) {
+                         still_conflict = true;
+                         break;
+                     }
+                     if (req.txn_id_ == txn->get_transaction_id() && req.lock_mode_ == LockMode::SHARED) {
+                         still_conflict = true;
+                         break;
+                     }
+                 }
+            }
 
-        //     if (!still_conflict) {
-        //         current_request_it->granted_ = true;
-        //         txn->get_lock_set()->insert(lock_data_id);
-        //         return true;
-        //     }
-        // }
+            if (!still_conflict) {
+                current_request_it->granted_ = true;
+                txn->get_lock_set()->insert(lock_data_id);
+                return true;
+            }
+        }
     }
 }
 
