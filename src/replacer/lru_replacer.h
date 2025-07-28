475228e07a19bb5934eaa46334046f412c6dbf38
Masttf <111647759+Masttf@users.noreply.github.com>
2025-07-28 03:35:42 +0000

    style: format code with clang-format

--- src/replacer/lru_replacer.h
+++ src/replacer/lru_replacer.h
@@ -58,7 +58,7 @@ class LRUReplacer : public Replacer {
      * 3. 避免在链表中的线性查找
      */
     std::vector<std::list<frame_id_t>::iterator> LRUhash_;  // 哈希表，存储frame_id到链表节点的映射
-    std::vector<bool> is_pinned_;  // 哈希表的使用标记，避免重复插入
-    
+    std::vector<bool> is_pinned_;                           // 哈希表的使用标记，避免重复插入
+
     size_t max_size_;  // 最大容量，与缓冲池容量相同
 };
